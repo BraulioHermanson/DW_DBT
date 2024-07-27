e0aa573ff892e2e009ce1398e1a9c8a0b5383100
BraulioHermanson <brauliohcampos0@outlook.com>
2024-07-27 13:55:01 -0300

    feat: adicionado postgres Render e conexao

--- src/extract_load.py
+++ src/extract_load.py
@@ -5,26 +5,40 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
-#TODO import das variaveis de ambiente
-
+load_dotenv()
 #TODO pegar a cotacao dos ativos
 commodities = ['CL=F', 'GC=F', 'SI=F']
 
-def buscar_dados_commodities(ticker, periodo='5d', intervalo='1d'):
-    ticker = yf.Ticker('CL=F')
+#TODO import das variaveis de ambiente
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine =  create_engine(DATABASE_URL)
+
+
+def buscar_dados_commodities(simbolo, periodo='5d', intervalo='1d'):
+    ticker = yf.Ticker(simbolo)
     dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
-    dados['Ticker'] = ticker
+    dados['simbolo'] = simbolo
     return dados
 
-def concat_dados(commodities):
-    data = []
-    for ticker in commodities:
-        dados = buscar_dados_commodities(ticker)
-        data.append(dados)
-    return pd.concat(data)
+def buscar_todos_dados_commodities(commodities):
+    todos_dados = []
+    for simbolo in commodities:
+        dados = buscar_dados_commodities(simbolo)
+        todos_dados.append(dados)
+    return pd.concat(todos_dados)
+
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
 
 if __name__ == "__main__":
-    concat_dados = buscar_dados_commodities(commodities)
-    print(concat_dados)
+    dados_concatenados = buscar_todos_dados_commodities(commodities)
+    salvar_no_postgres(dados_concatenados, schema='public')
 
-#TODO salvar no banco de dados
